65285e0242697dddd44452c7b3b1098df03dac03
Stijn Dekker <stijn.dekker@gmail.com>
2025-06-28 08:19:38 +0200

    A simplified intropage

--- intropage.tpl.php
+++ intropage.tpl.php
@@ -1,36 +1,33 @@
-<?php $enmoties = ($show_motions) ? ' en moties ': ''; ?>
-<?php $ofmoties = ($show_motions) ? ' of moties ': ''; ?>
 <div class="ammo-content">
-  <h1>Wijzigingsvoorstellen<?php print $enmoties; ?></h1>
+  <h1>Wijzigingsvoorstellen en moties</h1>
+ 
   <?php if ($has_any_access): ?>
-    <p><strong>U kunt in dit scherm:</strong></p>
-    <ul>
-      <li>Een overzicht van <?php print l('wijzigingsvoorstellen bekijken', 'ammo/amendments'); ?><?php print ($show_motions) ? ' of ' . l('moties', 'ammo/motions') : ''; ?> .</li>
-      <?php if ($add_access): ?>
-        <li>Nieuwe <?php print l('wijzigingsvoorstellen', 'ammo/amendment'); ?><?php print ($show_motions) ? ' en ' . l('moties', 'ammo/motion') : ''; ?> indienen.</li>
-      <?php endif; ?>
-      <?php if ($superadmin_access): ?>
-        <li>Een nieuwe <?php print l('bijeenkomst toevoegen', 'ammo/meeting'); ?>.</li>
-        <li>Een nieuw <?php print l('stuk toevoegen', 'ammo/document'); ?> bij een bijeenkomst.</li>
-      <?php endif; ?>
-    </ul>
-    <?php if ($add_access): ?>
-      <p><strong>In een overzicht kunt u:</strong></p>
-      <ul>
-        <li>Wijzigingsvoorstellen<?php print $enmoties; ?> van andere afdelingen mede indienen.</li>
-        <?php if (!$admin_access): ?>
-          <li>Uw ingediende wijzigingsvoorstellen<?php print $enmoties; ?> aanpassen.</li>
-        <?php endif; ?>
-        <?php if ($admin_access): ?>
-          <li>Ingediende wijzigingsvoorstellen<?php print  $enmoties; ?> aanpassen.</li>
-          <li>Ingediende wijzigingsvoorstellen<?php print  $enmoties; ?> voorzien van advies.</li>
-          <li>De status van ingediende wijzigingsvoorstellen<?php $enmoties; ?> aanpassen.</li>
-        <?php endif; ?>
-        <li>Uw ingediende wijzigingsvoorstellen<?php print $enmoties; ?> intrekken.</li>
-      </ul>
-      <?php endif; ?>
-    <p><strong>Klik op een van de grijze tabjes bovenaan voor de gewenste functies.</strong></p>
+
+    <div class="ammo-section-content">
+      <h2>Bekijken</h2>
+      <p>Voordat je een voorstel of motie indient, bekijk je eerst het overzicht van de ingediende voorstellen. Vaak kun je bij een bestand voorstel aansluiten, hier kun je in het overzicht steun bij aangeven.</p>
+      <nav class="button-group">
+        <ul>
+          <li><a href="ammo/amendments">Overzicht wijzigingsvoorstellen</a></li>
+          <li><a href="ammo/motions">Overzicht moties</a></li>
+        </ul>
+      </nav>
+    </div>
+
+    <div class="ammo-section-content">   
+      <h2>Indienen</h2>
+      <p>Als er geen voorstellen zijn waar je bij kunt aansluiten kun je namens je afdeling(en) een wijzigingsvoorstel of motie indienen. Deze moeten besproken zijn tijdens een ledenvergadering.</p>
+      <p><strong>Let op!</strong> Tekstcorrecties, typefouten, e.d. kunnen worden ingediend bij <a href="mailto:secretariaat@sp.nl">secretariaat@sp.nl </a></p>
+      <nav class="button-group">
+        <ul>
+          <li><a href="ammo/amendment">Wijzigingsvoorstel <span class="ammo-button-label">Voor inoudelijke toevoegingen en wijzigingen</span></a></li>
+          <li><a href="ammo/motion">Motie <span class="ammo-button-label">Om de partij op te roepen tot een specifiek actie</span></a></li>
+        </ul>
+      </nav>
+    </div>
+  
   <?php else: ?>
-    <p><?php print t('You do not have the necessary permissions to use this module.'); ?></p>
+      <p><?php print t('You do not have the necessary permissions to use this module.'); ?></p>
   <?php endif; ?>
+
 </div>
