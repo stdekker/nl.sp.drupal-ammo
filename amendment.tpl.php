1fb773a08edcf268c5758213b58f132c49b86491
Stijn Dekker <stijn.dekker@gmail.com>
2025-07-29 16:41:22 +0200

    Advice display consistency + admins can bypass stemmingsaanvraag datum

--- amendment.tpl.php
+++ amendment.tpl.php
@@ -85,7 +85,7 @@ if (!empty($vote_requests) && is_array($vote_requests) && count($vote_requests) 
 }
 ?>
 
-<?php if (!empty($advice) && (!$hide_advice || ($admin_access && empty($mail))) && (!empty($vote_requests) && is_array($vote_requests) && count($vote_requests) > 0)): ?>
+<?php if (!empty($advice) && (!$hide_advice || ($admin_access && empty($mail))) && ((!empty($vote_requests) && is_array($vote_requests) && count($vote_requests) > 0) || $state === 'rejected')): ?>
 <h4>Advies:</h4>
 <p>
 <?php
